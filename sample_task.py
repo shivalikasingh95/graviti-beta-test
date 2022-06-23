9d150b2901cfc0fca3bf9ef36eadf713ca992d84
shivalikasingh95 <73357305+shivalikasingh95@users.noreply.github.com>
2022-06-23 20:40:27 +0530

    Deleted sample task

--- sample_task.py
+++
@@ -1,9 +0,0 @@
-import logging
-import torch
-from PIL import Image
-from graviti import DataFrame, Workspace
-from graviti.utility import File
-import graviti.portex as pt
-from torchvision import transforms
-logging.basicConfig(level=logging.INFO)
-logging.info("hello taskAA!")
